ccdbf84fe3b72cbb642a761830ef28ae08ac39fc
Danylo <lliivadin@gmail.com>
2025-11-30 21:49:15 +0200

    fix: add labels

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,13 @@
 document.querySelectorAll('input').forEach((input) => {
-    const labelText = input.name.charAt(0).toUpperCase() + input.name.slice(1);
-    
-    input.placeholder = labelText; 
-    
-    const label = document.createElement('label');
-    label.textContent = labelText; 
-    label.classList.add('field-label');
-    label.setAttribute('for', input.id);
-    
-    input.parentElement.append(label);
-})
+  const labelText = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+
+  input.placeholder = labelText;
+
+  const label = document.createElement('label');
+
+  label.textContent = labelText;
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
+
+  input.parentElement.append(label);
+});
